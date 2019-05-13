9eaa91319c1e9f77aa8a26c10a06c00ef48961db
Cosmin Serban <serbancosmin2008@gmail.com>
2019-05-14 00:42:19 +0300

    initial commit

---
+++ ClassRoomNet5Reviews/ClassRoomNet5Reviews/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassRoomNet5Reviews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //week1
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
